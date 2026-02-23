026195c09b63852345e8d2decba7eba7c46e972f
MentaL0-main <obilev0000@gmail.com>
2026-02-23 10:40:07 +0300

    Create roadmap.cpp

---
+++ roadmap.cpp
@@ -0,0 +1,19 @@
+1) Complete projects in various languages ​​as part of learning these languages...................................................STARTED
+2) Organize GitHub. Add comments where necessary, split into files, do a full code review. Check README.md and LICENSE..........NOT STARTED
+3) Edit the resume. Completely redesign the entire project, but keep the same colors. Add new projects..........................NOT STARTED
+4) Deeply study Linux/Windows systems, Assembly, and C..........................................................................NOT STARTED
+5) Deeply study networking, the OSI model, and TCP/IP...........................................................................NOT STARTED
+6) Write a cross-platform chat on bare Windows/Linux sockets in pure C..........................................................NOT STARTED
+7) Implement a Telegram bot in Assembly.........................................................................................NOT STARTED
+8) Write your own allocator in C................................................................................................NOT STARTED
+9) Write a raycasting game in Assembly..........................................................................................NOT STARTED
+10) Learn English to level C1...................................................................................................NOT STARTED
+11) Learn reverse engineering and reverse engineer a lot........................................................................NOT STARTED
+12) Master security tools: Wireshark, Nmap, Burp Suite, and others..............................................................NOT STARTED
+13) Learn cryptography..........................................................................................................NOT STARTED
+14) Learn DevOps and CI/CD......................................................................................................NOT STARTED
+15) Contribute to open source projects. Make over 30 pull requests in various repositories. Only merge pools are counted........NOT STARTED
+16) Research numerous open vulnerabilities......................................................................................NOT STARTED
+17) Create a secure messenger in Asm/C/C++. Completely private, accessible to a select group of people..........................NOT STARTED
+18) Participate in hackathons, ctf, and related events..........................................................................NOT STARTED
+19) Conduct hacks on five of my own objects (games, websites, applications) solely for legitimate purposes......................NOT STARTED
