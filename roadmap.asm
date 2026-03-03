903700d8d551a8438e5e6025fe84ff7a349b78f6
MentaL0-main <obilev0000@gmail.com>
2026-03-03 10:59:01 +0300

    Update roadmap.asm

--- roadmap.asm
+++ roadmap.asm
@@ -2,23 +2,24 @@
 2) Organize GitHub. Add comments where necessary, split into files, do a full code review. Check README.md and LICENSE..........ENDED
 3) Edit the resume. Completely redesign the entire project, but keep the same colors. Add new projects..........................ENDED
 4) Deeply study Linux/Windows systems, Algorithms, Assembly, and C..............................................................STARTED
-5) Deeply study networking, the OSI model, and TCP/IP...........................................................................NOT STARTED
-6) Write a cross-platform chat on bare Windows/Linux sockets in pure C..........................................................NOT STARTED
-7) Implement a Telegram bot in Assembly.........................................................................................NOT STARTED
-8) Write your own allocator in C................................................................................................NOT STARTED
-9) Write a raycasting game in Assembly..........................................................................................NOT STARTED
-10) Learn English to level C1...................................................................................................NOT STARTED
-11) Learn reverse engineering and reverse engineer a lot........................................................................NOT STARTED
-12) Master security tools: Wireshark, Nmap, Burp Suite, and others..............................................................NOT STARTED
-13) Learn cryptography..........................................................................................................NOT STARTED
-14) Learn DevOps and CI/CD......................................................................................................NOT STARTED
-15) Contribute to open source projects. Make over 30 pull requests in various repositories. Only merge pools are counted........NOT STARTED
-16) Research numerous open vulnerabilities......................................................................................NOT STARTED
-17) Create a secure messenger in Asm/C/C++. Completely private, accessible to a select group of people..........................NOT STARTED
-18) Participate in hackathons, ctf, and related events..........................................................................NOT STARTED
-19) Conduct hacks on five of my own objects (games, websites, applications) solely for legitimate purposes......................NOT STARTED
-20) Write a Kernel-Mode Rootkit (Linux or Windows)..............................................................................NOT STARTED
-21) Develop an Evasion Framework (AV/EDR Bypass)................................................................................NOT STARTED
-22) Build Your Own Interpreter for a Custom DSL.................................................................................NOT STARTED
+5) Develop a product with monetization..........................................................................................STARTED
+6) Deeply study networking, the OSI model, and TCP/IP...........................................................................NOT STARTED
+7) Write a cross-platform chat on bare Windows/Linux sockets in pure C..........................................................NOT STARTED
+8) Implement a Telegram bot in Assembly.........................................................................................NOT STARTED
+9) Write your own allocator in C................................................................................................NOT STARTED
+10) Write a raycasting game in Assembly..........................................................................................NOT STARTED
+11) Learn English to level C1...................................................................................................NOT STARTED
+12) Learn reverse engineering and reverse engineer a lot........................................................................NOT STARTED
+13) Master security tools: Wireshark, Nmap, Burp Suite, and others..............................................................NOT STARTED
+14) Learn cryptography..........................................................................................................NOT STARTED
+15) Learn DevOps and CI/CD......................................................................................................NOT STARTED
+16) Contribute to open source projects. Make over 30 pull requests in various repositories. Only merge pools are counted........NOT STARTED
+17) Research numerous open vulnerabilities......................................................................................NOT STARTED
+18) Create a secure messenger in Asm/C/C++. Completely private, accessible to a select group of people..........................NOT STARTED
+19) Participate in hackathons, ctf, and related events..........................................................................NOT STARTED
+20) Conduct hacks on five of my own objects (games, websites, applications) solely for legitimate purposes......................NOT STARTED
+21) Write a Kernel-Mode Rootkit (Linux or Windows)..............................................................................NOT STARTED
+22) Develop an Evasion Framework (AV/EDR Bypass)................................................................................NOT STARTED
+23) Build Your Own Interpreter for a Custom DSL.................................................................................NOT STARTED
 
 Statuses: NOT STARTED, PLANNED, STARED, ENDED.
